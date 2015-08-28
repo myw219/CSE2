861e71cd1dc4cfa72127a5a7c0b0508f36ed9405
myw219 <mattnj58@gmail.com>
2015-08-28 19:08:08 +0000

    updated-Lab01

---
+++ Lab01/Helloworld.java
@@ -0,0 +1,26 @@
+//Matt Wong
+//Lab01
+//Hello World!
+//
+//  first compile the program 
+//      javac.Helloworld.java
+//  run the program
+//      java Helloworld//
+
+// define the class
+public class Helloworld 
+{
+    //add method
+    public static void main(String[] args)
+    {
+        //the messsage
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("#       Hello World!  #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+    }
+}
