4311e3ed239b3dbc4e97f0438c7deaeb3d2effb3
myw219 <mattnj58@gmail.com>
2015-08-30 19:15:46 +0000

    hand in homework

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Matthew Wong
+//hw01
+//WelcomeClass
+//
+//Create the class
+public class WelcomeClass
+{
+    //shows the message
+    public static void main(String[] args)
+    {
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/\\  /\\ /\\ /\\ /\\ /\\");
+        System.out.println("<-M--Y--W--2--1--9->");
+        System.out.println("\\/ \\/ \\/ \\/ \\/ \\/");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("this August my wife and I took an eleven-day,675-mile bike");
+        System.out.println("tour on our tandem. We circled Lake Ontario");
+    }
+}
